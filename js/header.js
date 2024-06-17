c262c2b8e9d639d6376e48fb7d813bc6b59ce20b
hyenggg <hwyoon022@naver.com>
2024-06-17 09:03:49 +0900

    적응형웹

---
+++ js/header.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  const BODY = $("body");
+  const mobBtn = $(".mob_btn");
+  const scrollTopBtn = $('.scrollTop_btn');
+
+  //(2)Mobile Menu
+
+  mobBtn.on("click",function () {
+    BODY.toggleClass("mOpen");
+  });
+});
